aa16a681b13fc0bae6a16eddaa52e4facf183b0c
Andres <andres_rochag@hotmail.com>
2020-06-16 20:25:54 -0500

    Third App
    Season app was created and the idea is to use Class-Based Components and States

---
+++ thirdapp/seasons/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        //Use setState only
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
+  }
+  // React says we have to define render!!!
+  render() {
+    return <div>Latitud: {this.state.lat} </div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
