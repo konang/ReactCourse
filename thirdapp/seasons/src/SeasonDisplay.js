548009c18873a746a58af79eb673234f60679110
Andres <andres_rochag@hotmail.com>
2020-06-16 22:14:58 -0500

    Third app finalized
    Finished the third app where based on geolocation and current time determines if it is summer or winter and displays it with a loader asking to accept the app to access user location.

--- thirdapp/seasons/src/SeasonDisplay.js
+++ thirdapp/seasons/src/SeasonDisplay.js
@@ -1,7 +1,35 @@
 import React from "react";
+import "./SeasonDisplay.css";
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Brr it's Chilly",
+    iconName: "snowflake",
+  },
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat < 0 ? "summer" : "winter";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season]; //{text, iconName}
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`}></i>
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`}></i>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- thirdapp/seasons/src/index.js
+++ thirdapp/seasons/src/index.js
@@ -1,22 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { lat: null };
+  //This is the only time direct assignment is used for state
+  state = { lat: null, errorMessage: "" };
 
+  componentDidMount() {
+    console.log("My component was rendered to the screen");
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        //Use setState only
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => console.log(err)
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
+  }
+
+  componentDidUpdate() {
+    console.log("My component was updated and rerendered");
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <Spinner message="Please accept loaction request" />;
   }
   // React says we have to define render!!!
   render() {
-    return <div>Latitud: {this.state.lat} </div>;
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
