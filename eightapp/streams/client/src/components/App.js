f8862eb68a5844cb2ac350b195e2b216f377798f
Andres <andres_rochag@hotmail.com>
2020-06-19 06:13:48 -0500

    Header with links and different pages
    Created several starting pages and routes to them as well as a header with links

--- eightapp/streams/client/src/components/App.js
+++ eightapp/streams/client/src/components/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import Header from "./Header";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <Header></Header>
+        <div>
+          <Route path="/" exact component={StreamList}></Route>
+          <Route path="/streams/create" component={StreamCreate}></Route>
+          <Route path="/streams/show" component={StreamShow}></Route>
+          <Route path="/streams/edit" component={StreamEdit}></Route>
+          <Route path="/streams/delete" component={StreamDelete}></Route>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
