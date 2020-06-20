4b0d462271b40edb8b5763fb377c06a20a8a8664
Andres <andres_rochag@hotmail.com>
2020-06-20 06:47:31 -0500

    Finished all the data management of the 8th app

--- eightapp/streams/client/src/components/App.js
+++ eightapp/streams/client/src/components/App.js
@@ -1,25 +1,26 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import Header from "./Header";
+import history from "../history";
 
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <Header></Header>
         <div>
           <Route path="/" exact component={StreamList}></Route>
-          <Route path="/streams/create" component={StreamCreate}></Route>
-          <Route path="/streams/show" component={StreamShow}></Route>
-          <Route path="/streams/edit" component={StreamEdit}></Route>
-          <Route path="/streams/delete" component={StreamDelete}></Route>
+          <Route path="/streams/new" component={StreamCreate}></Route>
+          <Route path="/streams/show/:id" component={StreamShow}></Route>
+          <Route path="/streams/edit/:id" component={StreamEdit}></Route>
+          <Route path="/streams/delete/:id" component={StreamDelete}></Route>
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
