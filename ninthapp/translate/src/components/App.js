58c92676dc4b267764a907da2ebaebe3a64af5f1
Andres <andres_rochag@hotmail.com>
2020-06-21 04:25:22 -0500

    9th Language app using context instead of Redux

---
+++ ninthapp/translate/src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import UserCreate from "./UserCreate";
+import { LanguageStore } from "../context/LanguageContext";
+import ColorContext from "../context/ColorContext";
+import LanguageSelector from "./LanguageSelector";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="ui container">
+        <LanguageStore>
+          <LanguageSelector></LanguageSelector>
+
+          <ColorContext.Provider value="red">
+            <UserCreate></UserCreate>
+          </ColorContext.Provider>
+        </LanguageStore>
+      </div>
+    );
+  }
+}
+
+export default App;
